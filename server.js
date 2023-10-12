4661ba20243c310c9e28e1d2968f23224d03bc13
AnselOng <85985021+AnselOng@users.noreply.github.com>
2023-10-13 01:05:51 +0800

    Add files via upload
    Initial Commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const shortid = require('shortid');
+
+let _urls = fs.readFileSync('urls.json');
+let urls = JSON.parse(_urls);
+
+console.log(urls);
+
+function validateUrl(value) {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+	    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+	    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+	    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+	    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+	    '(\\#[-a-z\\d_]*)?$','i');
+
+      return !!urlPattern.test(value);
+}
+
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended : false}));
+
+app.get('/', async (req, res) => {
+    //const shortUrls = await ShortUrl.find();
+    //res.render('index', { shortUrls: shortUrls });
+    res.render('index', {urls: urls});
+});
+
+app.post('/shortUrl', async (req, res) => {
+    // await ShortUrl.create({ full: req.body.fullUrl });
+    // res.redirect('/');
+    let newUrl = {
+        "full" : "",
+        "short": ""
+    };
+
+    let found = false;
+    if (validateUrl(req.body.fullUrl)) {
+        try {
+            for(let i = 0; i < urls.length; i++) {
+                if(urls[i].full == req.body.fullUrl)
+                found = true;
+            }
+            if (found) {
+                //Exists, ignore and skip
+                //res.json(url);
+                console.log('exists: ', req.body.fullUrl);
+            } else {
+                //Does not exist, add into json file
+                newUrl.full = req.body.fullUrl
+                newUrl.short = shortid.generate();
+                urls.push(newUrl);
+                var newUrlObj = JSON.stringify(urls);
+
+                fs.writeFile('urls.json', newUrlObj, err => {
+                    // error checking
+                    if(err) throw err;
+                    console.log("New data added");
+                });
+                res.redirect('/');
+            }
+        } catch (err) {
+          console.log(err);
+          res.status(500).json('Server Error');
+        }
+      } else {
+        res.status(400).json('Invalid Original Url');
+      }
+})
+
+app.get('/:shortUrl', (req, res) => {
+    for(let i = 0; i < urls.length; i++) {
+        if(urls[i].short === req.params.shortUrl){
+            res.redirect(urls[i].full);
+        }
+    }
+})
+
+app.listen(process.env.port || 5000);
